4aef035091326d34906f0199b70592224d490844
Danny Goodacre <danny.goodacre@bath.edu>
2026-08-13 23:41:04 +0100

    WIP

--- frontend/src/components/TradeForm/Header.styles.ts
+++ frontend/src/components/TradeForm/Header.styles.ts
@@ -6,11 +6,9 @@ export const formHeader = {
   flexDirection: 'column',
 };
 
-export const formRow = (theme: Theme) => {
-  return {
-    display: 'flex',
-    gap: theme.spacing(2),
-    marginBottom: theme.spacing(4),
-    width: '100%',
-  };
-};
+export const formRow = (theme: Theme) => ({
+  display: 'flex',
+  gap: theme.spacing(2),
+  marginBottom: theme.spacing(4),
+  width: '100%',
+});
